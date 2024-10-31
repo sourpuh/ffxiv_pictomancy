6fd395317f26468ced1845dc221c28a83d95d23b
sourpuh <150582765+sourpuh@users.noreply.github.com>
2024-10-31 12:23:35 -0700

    Add soul gauge clip

--- Pictomancy/DXDraw/AddonClipper.cs
+++ Pictomancy/DXDraw/AddonClipper.cs
@@ -577,10 +577,11 @@ internal class AddonClipper
         ClipAtkNodeDiamond(addon->UldManager.NodeList[13], scale: _diamondScale);
         ClipAtkNodeDiamond(addon->UldManager.NodeList[14], scale: _diamondScale);
         ClipAtkNodeCircle(addon->UldManager.NodeList[5], scale: _gaugeTextScale);
+        ClipAtkNodeRectangle(addon->UldManager.NodeList[16]->GetAsAtkComponentNode()->Component->UldManager.NodeList[0], scale: _resourceBarScale);
+        ClipAtkNodeCircle(addon->UldManager.NodeList[17], scale: _gaugeTextScale);
     }
     private unsafe void ClipMch()
     {
-
         var addon = GetVisibleAddonOrNull("JobHudMCH0", 10);
         if (addon == null) return;
         ClipAtkNodeRectangle(addon->UldManager.NodeList[3]->GetAsAtkComponentNode()->Component->UldManager.NodeList[0], scale: _resourceBarScale);
